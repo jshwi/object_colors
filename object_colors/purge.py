4dc656a52550831d5c02159a3cb48503382a401b
jshwi <stephen@jshwisolutions.com>
2020-02-07 21:08:54 +1100

    WIP

---
+++ object_colors/purge.py
@@ -0,0 +1,147 @@
+# import configparser
+# import os
+# import shutil
+# import subprocess
+import sys
+from configparser import ConfigParser
+from os import path, rename, listdir
+from shutil import rmtree, which
+from subprocess import Popen, PIPE
+from typing import Optional
+
+import setuptools
+
+from object_colors import Color
+
+
+class Purge(setuptools.Command):
+    """Delete ALL python artifacts"""
+
+    user_options = [("ignore=", "i", "keep entered files and dirs")]
+
+    # noinspection PyAttributeOutsideInit
+    def initialize_options(self):
+        """Abstract method that is required to be overwritten"""
+        self.ignore = None
+
+    def finalize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    @staticmethod
+    def __parse_config() -> Optional[str]:
+        # to use python setup.py purge without deleting virtual
+        # environment add:
+        #
+        # [DEFAULT]
+        # venv=<virtual environment>
+        #
+        # to a config file called `venv.ini`
+        config = ConfigParser()
+        config.read("venv.ini")
+        if config["DEFAULT"] and config["DEFAULT"]["venv"]:
+            return config["DEFAULT"]["venv"]
+        return None
+
+    @staticmethod
+    def __color_class() -> Color:
+        color = Color()
+        color.populate_colors()
+        return color
+
+    def __clean_repo(self) -> None:
+        # list of python artifacts and distribution sources to delete
+        # will delete ALL of these items in the repo - recursively
+        color = self.__color_class()
+        files = [
+            '"build"',
+            '"dist"',
+            '"__pycache__"',
+            '"pip-wheel-metadata"',
+            '"htmlcov"',
+            '".coverage"',
+            '"*.pyc"',
+            '"*.egg*"',
+            '"*.pyo"',
+            '"*~"',
+        ]
+        for key in files:
+            command = f'find . -name {key} -exec rm -rf {{}} + -print'
+            key = color.green.bold.get("Executing:")
+            val = color.yellow.get(command)
+            print(f"{key} {val}")
+            with Popen(
+                    command,
+                    shell=True,
+                    stdout=PIPE,
+                    bufsize=1,
+                    universal_newlines=True
+            ) as out:
+                for line in out.stdout:
+                    print(line, end="")
+
+    @staticmethod
+    def __return(exclude: str, up_dir: str) -> None:
+        # return the preserved virtual environment to the repo root
+        # directory
+        if path.isdir(up_dir):
+            rename(up_dir, exclude)
+
+    def __resolve_opt(self, exclude: str, up_dir: str) -> None:
+        # if choosing to save the virtual environment take it out
+        # of the repo root temporarily as removing python cache
+        # files from it will render it completely useless
+        # otherwise simply delete it
+        if path.isdir(exclude):
+            if self.ignore:
+                rmtree(exclude)
+            else:
+                rename(exclude, up_dir)
+
+    def __detect_venv(self) -> Optional[str]:
+        if hasattr(sys, 'real_prefix'):
+            pythonpath = which("python")
+            return path.dirname(path.dirname(pythonpath))
+        return self.__detect_venv_cfg()
+
+    @staticmethod
+    def __detect_venv_cfg() -> Optional[str]:
+        repo = listdir(".")
+        for item in repo:
+            if path.isdir(item):
+                files = listdir(item)
+                if "pyvenv.cfg" in files:
+                    return item
+        return None
+
+    @staticmethod
+    def __write_config(venv: str):
+        config = ConfigParser()
+        config["DEFAULT"] = {"venv": venv}
+        with open("venv.ini", "w") as file:
+            config.write(file)
+
+    def __write_ini(self):
+        color = self.__color_class()
+        venv = self.__detect_venv()
+        if venv:
+            color.green.bold.print("Virtual Environment Detected")
+            color.yellow.print_key(
+                f"{venv} appears to be your current virtual environment.",
+                venv
+            )
+            choice = input("Is this correct? [Y/n]").lower()
+            if choice == "n":
+                venv = None
+        if not venv:
+            venv = input(
+                "Please enter the name of your virtual environment."
+            )
+        self.__write_config(venv)
+
+    def run(self) -> None:
+        exclude = self.__parse_config()
+        up_dir = path.join("..", exclude)
+        self.__resolve_opt(exclude, up_dir)
+        self.__parse_config()
+        self.__clean_repo()
+        self.__return(exclude, up_dir)
--- setup.py
+++ setup.py
@@ -1,107 +1,10 @@
-import configparser
-import os
-import shutil
-import subprocess
-from typing import Optional
-from object_colors import Color
-
+#!/usr/bin/env python3
 import setuptools
 
 
 def readme():
     with open("README.rst", "r") as file:
         return file.read()
-
-
-class Purge(setuptools.Command):
-    """Delete ALL python artifacts"""
-
-    user_options = [("ignore=", "i", "keep entered files and dirs")]
-
-    # noinspection PyAttributeOutsideInit
-    def initialize_options(self):
-        """Abstract method that is required to be overwritten"""
-        self.ignore = None
-
-    def finalize_options(self):
-        """Abstract method that is required to be overwritten"""
-
-    @staticmethod
-    def __parse_config() -> Optional[str]:
-        # to use python setup.py purge without deleting virtual
-        # environment add:
-        #
-        # [DEFAULT]
-        # venv=<virtual environment>
-        #
-        # to a config file called `venv.ini`
-        config = configparser.ConfigParser()
-        config.read("venv.ini")
-        if config["DEFAULT"] and config["DEFAULT"]["venv"]:
-            return config["DEFAULT"]["venv"]
-        return None
-
-    @staticmethod
-    def __clean_repo() -> None:
-        # list of python artifacts and distribution sources to delete
-        # will delete ALL of these items in the repo - recursively
-        files = [
-            '"build"',
-            '"dist"',
-            '"__pycache__"',
-            '"pip-wheel-metadata"',
-            '"htmlcov"',
-            '".coverage"',
-            '"*.pyc"',
-            '"*.egg*"',
-            '"*.pyo"',
-            '"*~"',
-        ]
-        color = Color()
-        color.populate_colors()
-        for key in files:
-            command = f'find . -name {key} -exec rm -rf {{}} + -print'
-            key = color.green.bold.get("Executing:")
-            val = color.yellow.get(command)
-            print(f"{key} {val}")
-            with subprocess.Popen(
-                    command,
-                    shell=True,
-                    stdout=subprocess.PIPE,
-                    bufsize=1,
-                    universal_newlines=True
-            ) as out:
-                for line in out.stdout:
-                    print(line, end="")
-
-    @staticmethod
-    def __return(exclude: str, up_dir: str) -> None:
-        # return the preserved virtual environment to the repo root
-        # directory
-        if os.path.isdir(up_dir):
-            os.rename(up_dir, exclude)
-
-    def __resolve_opt(self, exclude: str, up_dir: str) -> None:
-        # if choosing to save the virtual environment take it out
-        # of the repo root temporarily as removing python cache
-        # files from it will render it completely useless
-        # otherwise simply delete it
-        if os.path.isdir(exclude):
-            if self.ignore:
-                shutil.rmtree(exclude)
-            else:
-                os.rename(exclude, up_dir)
-
-    def run(self) -> None:
-        """Parse virtual environment name from config file
-        Opt will toggle whether to delete virtual environment or not
-        """
-        exclude = self.__parse_config()
-        up_dir = os.path.join("..", exclude)
-        self.__resolve_opt(exclude, up_dir)
-        self.__parse_config()
-        self.__clean_repo()
-        self.__return(exclude, up_dir)
 
 
 setuptools.setup(
